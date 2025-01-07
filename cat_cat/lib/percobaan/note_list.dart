d4dc75bb00cd02b552af187b4c27b10015b32f2f
Zufaroro <129752885+Zufaroro@users.noreply.github.com>
2025-01-07 18:39:35 +0700

    notekeeper_6.02
    https://www.youtube.com/playlist?list=PLDQl6gZtjvFu5l20K5KTEBLCjfRjHowLj

---
+++ cat_cat/lib/percobaan/note_list.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class NoteList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return NoteListState();
+  }
+}
+
+class NoteListState extends State<NoteList> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: getNoteListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('FAB is Presed');
+        },
+        tooltip: 'add note',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  ListView getNoteListView() {
+    TextStyle titleStyle = Theme.of(context).textTheme.subhead;
+
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.amber[50],
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.red[400],
+              child: Icon(Icons.account_circle_outlined),
+            ),
+            title: Text(
+              'ikhwan',
+              style: titleStyle,
+            ),
+            subtitle: Text('27 septermber 2024'),
+            trailing: Icon(
+              Icons.delete,
+              color: Colors.grey,
+            ),
+            onTap: () {
+              debugPrint('List Tile Is Tapped');
+            },
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ cat_cat/lib/percobaan/notekeeper.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/percobaan/note_list.dart'
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'NoteKeeper',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.amber
+      ),
+      home: NoteList(),
+    );
+  }
+}
